07c5ac7747a719f556f0a6e5e6a935e4c6a3ab3b
Karol <karol.garlacz@pwrteams.com>
2025-07-22 08:22:24 +0200

    chore: added simple POM

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   workers: undefined,
   reporter: 'html',
   use: {
+    baseURL: 'http://localhost:3000',
     actionTimeout: 0,
     trace: 'on-first-retry',
     video: 'retain-on-failure',
