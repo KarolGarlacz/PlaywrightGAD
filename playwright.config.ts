7935238034a000158a2f9cf515b6dfe603760e2d
Karol <karol.garlacz@pwrteams.com>
2025-07-23 08:36:06 +0200

    feat: dotenv config setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,13 +5,14 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  globalSetup: require.resolve('./src/global-setup.ts'),
   timeout: 60_000,
   expect: { timeout: 10_000 },
   fullyParallel: true,
   workers: undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL,
     actionTimeout: 0,
     trace: 'on-first-retry',
     video: 'retain-on-failure',
