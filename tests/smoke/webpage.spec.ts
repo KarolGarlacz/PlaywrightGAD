a0cce663b82b38e7cd86925400f67d5322fc9458
Karol <karol.garlacz@pwrteams.com>
2025-08-13 13:08:40 +0200

    chore: added alias to improve imports

--- tests/smoke/webpage.spec.ts
+++ tests/smoke/webpage.spec.ts
@@ -1,6 +1,6 @@
-import { ArticlesPage } from '../../src/pages/articles.page';
-import { CommentsPage } from '../../src/pages/coments.page';
-import { HomePage } from '../../src/pages/home.page';
+import { ArticlesPage } from '@_src/pages/articles.page';
+import { CommentsPage } from '@_src/pages/coments.page';
+import { HomePage } from '@_src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
 test('home page title @smoke', async ({ page }) => {
