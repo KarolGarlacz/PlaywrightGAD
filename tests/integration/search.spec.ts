32376ca9ef66a2293420f005a9451cf92f0d1d0c
Karol <karol.garlacz@pwrteams.com>
2025-08-20 19:52:52 +0200

    test: go button action with API

---
+++ tests/integration/search.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
+import { ArticlesPage } from '@_src/pages/articles.page';
+
+test.describe('Verify search component for articles', () => {
+  test('go button should fetch articles @GAD-R07-01', async ({ page }) => {
+    // Arrange
+    const expectedDefaultArticleNumber = 6;
+    const articlesPage = new ArticlesPage(page);
+    articlesPage.goto();
+    const responsePromise = page.waitForResponse('/api/articles*');
+    // Act
+    await articlesPage.goSearchButton.click();
+    const response = await responsePromise;
+    const body = await response.json();
+    // Assert
+    expect(response.ok()).toBeTruthy();
+    expect(body).toHaveLength(expectedDefaultArticleNumber);
+  });
+});
